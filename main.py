163a1a9f0a596da6b038570d85677f15eff02804
ibenhfaiedh <ibrahimbenhf@gmail.com>
2022-02-07 21:51:23 +0100

    fix voice assistant interactions

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-import subprocess
-import wolframalpha
 import pyttsx3
-import json
 from googletrans import Translator
 import speech_recognition as sr
 from docx import Document
 import datetime
-import wikipedia
-import webbrowser
 import os
-import winshell
-import pyjokes
 import smtplib
 import ctypes
-import time
-import requests
-from urllib.request import urlopen
+from tensor import hide
 
 language = 'en'
 
@@ -114,43 +105,7 @@ if __name__ == '__main__':
         # stored here in 'query' and will be
         # converted to lower case for easily
         # recognition of command
-        if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=3)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
-
-        elif 'open youtube' in query:
-            speak("Here you go to Youtube\n")
-            webbrowser.open("youtube.com")
-
-        elif 'open google' in query:
-            speak("Here you go to Google\n")
-            webbrowser.open("google.com")
-
-        elif 'open stackoverflow' in query:
-            speak("Here you go to Stack Over flow.Happy coding")
-            webbrowser.open("stackoverflow.com")
-
-        elif 'play music' in query or "play song" in query:
-            speak("Here you go with music")
-            # music_dir = "G:\\Song"
-            music_dir = "C:\\Users\\GAURAV\\Music"
-            songs = os.listdir(music_dir)
-            print(songs)
-            random = os.startfile(os.path.join(music_dir, songs[1]))
-
-        elif 'time' in query:
-            strTime = datetime.datetime.now().strftime("% H:% M:% S")
-            speak(f"Sir, the time is {strTime}")
-
-        elif 'open opera' in query:
-            codePath = r"C:\\Users\\GAURAV\\AppData\\Local\\Programs\\Opera\\launcher.exe"
-            os.startfile(codePath)
-
-        elif 'send a mail' in query:
+        if 'send a mail' in query:
             try:
                 speak("What should I say?")
                 content = takeCommand()
@@ -166,110 +121,26 @@ if __name__ == '__main__':
             speak("Thanks for giving me your time")
             exit()
 
-        elif 'joke' in query:
-            speak(pyjokes.get_joke())
-
-        elif "calculate" in query:
-
-            app_id = "Wolframalpha api id"
-            client = wolframalpha.Client(app_id)
-            indx = query.lower().split().index('calculate')
-            query = query.split()[indx + 1:]
-            res = client.query(' '.join(query))
-            answer = next(res.results).text
-            print("The answer is " + answer)
-            speak("The answer is " + answer)
-
-        elif 'search' in query or 'play' in query:
-
-            query = query.replace("search", "")
-            query = query.replace("play", "")
-            webbrowser.open(query)
-
-        elif 'power point presentation' in query:
-            speak("opening Power Point presentation")
-            power = r"C:\\Users\\GAURAV\\Desktop\\Minor Project\\Presentation\\Voice Assistant.pptx"
-            os.startfile(power)
-
-        elif 'change background' in query:
-            ctypes.windll.user32.SystemParametersInfoW(20,
-                                                       0,
-                                                       "Location of wallpaper",
-                                                       0)
-            speak("Background changed successfully")
-
-
-        elif 'news' in query:
-
-            try:
-                jsonObj = urlopen(
-                    '''https://newsapi.org / v1 / articles?source = the-times-of-india&sortBy = top&apiKey =\\times of India Api key\\''')
-                data = json.load(jsonObj)
-                i = 1
-
-                speak('here are some top news from the times of india')
-                print('''=============== TIMES OF INDIA ============''' + '\n')
-
-                for item in data['articles']:
-                    print(str(i) + '. ' + item['title'] + '\n')
-                    print(item['description'] + '\n')
-                    speak(str(i) + '. ' + item['title'] + '\n')
-                    i += 1
-            except Exception as e:
-
-                print(str(e))
-
-
         elif 'lock window' in query:
             speak("locking the device")
             ctypes.windll.user32.LockWorkStation()
 
-        elif 'shutdown system' in query:
-            speak("Hold On a Sec ! Your system is on its way to shut down")
-            subprocess.call('shutdown / p /f')
-
-        elif 'empty recycle bin' in query:
-            winshell.recycle_bin().empty(confirm=False, show_progress=False, sound=True)
-            speak("Recycle Bin Recycled")
-
-        elif "where is" in query:
-            query = query.replace("where is", "")
-            location = query
-            speak("User asked to Locate")
-            speak(location)
-            webbrowser.open("https://www.google.nl / maps / place/" + location + "")
-
-        #    elif "camera" in query or "take a photo" in query:
-        #       ec.capture(0, "Jarvis Camera ", "img.jpg")
-
-        elif "restart" in query:
-            subprocess.call(["shutdown", "/r"])
-
-        elif "log off" in query or "sign out" in query:
-            speak("Make sure all the application are closed before sign-out")
-            time.sleep(5)
-            subprocess.call(["shutdown", "/l"])
-
         elif "write a note" in query:
             speak("What should i write, sir")
             note = takeCommand()
-            file = open('jarvis.txt', 'w')
-            speak("Sir, Should i include date and time")
-            snfm = takeCommand()
-            if 'yes' in snfm or 'sure' in snfm:
-                strTime = datetime.datetime.now().strftime("% H:% M:% S")
-                file.write(strTime)
-                file.write(" :- ")
-                file.write(note)
-            else:
-                file.write(note)
+            file = open('vermera.txt', 'w')
+            strTime = datetime.datetime.now().strftime("% H:% M:% S")
+            file.write(strTime)
+            file.write(" :- ")
+            file.write(note)
 
         elif "show note" in query:
             speak("Showing Notes")
-            file = open("jarvis.txt", "r")
+            file = open("vermera.txt", "r")
             print(file.read())
             speak(file.read(6))
 
+        #changing language command
         elif "change" in query:
             if language == 'fr':
                 language='en'
@@ -282,48 +153,13 @@ if __name__ == '__main__':
                  engine.setProperty('voice', voices[0].id)
 
 
-        elif "weather" in query:
-
-            # Google Open weather website
-            # to get API of Open weather
-            api_key = "Api key"
-            base_url = "http://api.openweathermap.org / data / 2.5 / weather?"
-            speak(" City name ")
-            print("City name : ")
-            city_name = takeCommand()
-            complete_url = base_url + "appid =" + api_key + "&q =" + city_name
-            response = requests.get(complete_url)
-            x = response.json()
-
-            if x["cod"] != "404":
-                y = x["main"]
-                current_temperature = y["temp"]
-                current_pressure = y["pressure"]
-                current_humidiy = y["humidity"]
-                z = x["weather"]
-                weather_description = z[0]["description"]
-                print(" Temperature (in kelvin unit) = " + str(
-                    current_temperature) + "\n atmospheric pressure (in hPa unit) =" + str(
-                    current_pressure) + "\n humidity (in percentage) = " + str(
-                    current_humidiy) + "\n description = " + str(weather_description))
-
-            else:
-                speak(" City Not Found ")
-
-
-        elif "food" in query:
-            speak(
-                "do you want Nutritional Burger Nigger Mother Fucker aowouuu awouu awouu Nutritional Burger Nigger Mother Fucker"
-                "skrapapa werererereri")
-
-        elif "okay" in query:
+        elif "write" in query:
 
             document = Document()
             document.add_heading('fahd Title', 0)
             document.add_paragraph('hello world im newton world')
             document.save('test.docx')
             os.startfile('test.docx')
+        else:
+            hide() #call for method
 
-        # elif "" in query:
-        # Command go here
-        # For adding more commands
---
+++ tensor.py
@@ -0,0 +1,18 @@
+import re
+import pickle
+import pandas as pd
+from bs4 import BeautifulSoup
+import pkg_resources
+from symspellpy import SymSpell, Verbosity
+import numpy as np
+import random as rn
+import warnings
+warnings.filterwarnings("ignore")
+from tqdm import tqdm
+import tensorflow as tf
+
+
+#testing
+def hide():
+    print('hey')
+
